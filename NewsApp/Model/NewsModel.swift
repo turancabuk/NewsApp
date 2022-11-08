46addfebe895361352893c2b90a3f04e35e72ffa
turan <turan6677@gmail.com>
2022-11-08 22:51:18 +0300

    NewsModel created.

---
+++ NewsApp/Model/NewsModel.swift
@@ -0,0 +1,59 @@
+//
+//  NewsModel.swift
+//  NewsApp
+//
+//  Created by Turan Çabuk on 8.11.2022.
+//
+
+import Foundation
+
+// MARK: - Article
+struct Article: Codable {
+    let source: Source?
+    let author: String?
+    let title, articleDescription: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: Date?
+    let content: String?
+
+    enum CodingKeys: String, CodingKey {
+        case source, author, title
+        case articleDescription
+        case url, urlToImage, publishedAt, content
+    }
+}
+
+// MARK: - Source
+struct Source: Codable {
+    let id: JSONNull?
+    let name: String?
+}
+
+// MARK: - Encode/decode helpers
+
+class JSONNull: Codable, Hashable {
+
+    public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
+        return true
+    }
+
+    public var hashValue: Int {
+        return 0
+    }
+
+    public init() {}
+
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if !container.decodeNil() {
+            throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encodeNil()
+    }
+}
+
