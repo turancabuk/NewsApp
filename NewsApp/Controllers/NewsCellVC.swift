e8b95eb64048e81aba0193827855117697b0f683
turan <turan6677@gmail.com>
2022-11-08 21:19:45 +0300

    folder is complete. NewsCellVC created. Constant.swift created.

---
+++ NewsApp/Controllers/NewsCellVC.swift
@@ -0,0 +1,14 @@
+//
+//  NewsCellVC.swift
+//  NewsApp
+//
+//  Created by Turan Çabuk on 8.11.2022.
+//
+
+import UIKit
+
+class NewsCellVC: UITableViewCell {
+
+
+
+}
---
+++ NewsApp/Helper/Constant.swift
@@ -0,0 +1,11 @@
+//
+//  Constant.swift
+//  NewsApp
+//
+//  Created by Turan Çabuk on 8.11.2022.
+//
+//
+//  URL:    https://api.collectapi.com/news/getNewsfromRSS?data.rss_url=http%3A%2F%2Fwww.webrazzi.com%2Ffeed%2F
+//  apikey: 6p3C5QKQmEbOEhWwTCV0CI:1JgDZwRLNbgyfmpqH8WWTx
+
+import Foundation
